4395f37f5251177e900e349f619162f45de1a46c
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-06-06 15:43:48 -0300

    add icons

--- src/components/TopComponent.jsx
+++ src/components/TopComponent.jsx
@@ -1,17 +1,19 @@
 import { colors } from "../assets/styleVariables"
-
+import { NotificationsOff, TransactionIcon } from "./Icons"
 
 export default function TopComponent () {
     return (
-        <header className="flex justify-around p-8">
+        <header className="flex justify-around p-8 text-xs font-semibold">
             <section className={`w-2/4 border-r-2 border-${colors.color_secondaryL}`}>
-                <span>
-                    transaction
+                <span className="flex gap-2 items-center">
+                    <TransactionIcon/>
+                    Transaction
                 </span>
             </section>
             <section>
-                <span>
-                    notifications
+                <span className="flex gap-2 items-center">
+                    <NotificationsOff/>
+                    Notifications
                 </span>
             </section>
         </header>
