4dcaf38e5f8a93938c200623354adbdf2b0fc389
tomas_dev <97204775+tomasquinteros@users.noreply.github.com>
2023-06-18 14:57:25 -0300

    Update Market.jsx

--- src/components/Crypto/Market.jsx
+++ src/components/Crypto/Market.jsx
@@ -6,12 +6,12 @@ export default function Market({ market }) {
         <div>
             {
                 market.length > 0 ?
-                    <div className="flex items-center gap-8 flex-col">
+                    <div className="flex items-center gap-4 flex-col">
                         {
                             market[0].price_change_24h < 0 ? <p className="flex items-center text-red-600"><TrendingDown /> ${market[0].price_change_24h} </p> : <p className="flex items-center text-[#0A937E]"><TrackIcon /> ${market[0].price_change_24h}</p>
                         }
                         {
-                            market[0].price_change_percentage_24h < 0 ? <p className="flex items-center text-red-600"><TrendingDown /> {market[0].price_change_percentage_24h}% </p> : <p className="flex items-center text-[#0A937E]"><TrackIcon /> ${market[0].price_change_percentage_24h}%</p>
+                            market[0].price_change_percentage_24h < 0 ? <p className="flex items-center text-red-600"><TrendingDown /> {market[0].price_change_percentage_24h}% </p> : <p className="flex items-center text-[#0A937E]"><TrackIcon /> {market[0].price_change_percentage_24h}%</p>
                         }
                     </div>
                     : console.log()
